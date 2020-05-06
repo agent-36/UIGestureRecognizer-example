38e31cf696ea4c79881aa1538c6e457a228fca48
Ihwan <mynameisihwan@gmail.com>
2020-05-06 14:50:02 +0700

    Initial Commit

---
+++ gesture-sample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  gesture-sample
+//
+//  Created by Ihwan ID on 06/05/20.
+//  Copyright © 2020 Ihwan ID. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
