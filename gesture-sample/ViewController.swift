a1c293651f49248c349ee7e4b319ad3997285901
edo lorenza <edolrnz@gmail.com>
2020-05-06 21:37:08 +0700

    swipe gesture

--- gesture-sample/ViewController.swift
+++ gesture-sample/ViewController.swift
@@ -10,11 +10,47 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var swipeLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let leftSwipeGesture = UISwipeGestureRecognizer(target: self, action: #selector(leftSwipedByUser(_:)))
+             leftSwipeGesture.direction = .left
+             self.view.addGestureRecognizer(leftSwipeGesture)
+             
+             let rightSwipeGesture = UISwipeGestureRecognizer(target: self, action: #selector(rightSwipedByUser(_:)))
+             rightSwipeGesture.direction = .right
+             self.view.addGestureRecognizer(rightSwipeGesture)
+             
+             let topSwipeGesture = UISwipeGestureRecognizer(target: self, action: #selector(topSwipedByUser(_:)))
+             topSwipeGesture.direction = .up
+             self.view.addGestureRecognizer(topSwipeGesture)
+             
+             let bottomSwipeGesture = UISwipeGestureRecognizer(target: self, action: #selector(bottomSwipedByUser(_:)))
+             bottomSwipeGesture.direction = .down
+             self.view.addGestureRecognizer(bottomSwipeGesture)
     }
 
+    
+    
+    @objc func leftSwipedByUser(_ gesture:UISwipeGestureRecognizer) {
+           self.swipeLabel.text = "Left Swipe"
+       }
+       
+       @objc func rightSwipedByUser(_ gesture:UISwipeGestureRecognizer) {
+           self.swipeLabel.text = "Right Swipe"
+       }
+       
+       @objc func topSwipedByUser(_ gesture:UISwipeGestureRecognizer) {
+           self.swipeLabel.text = "UP Swipe"
+       }
+       
+       @objc func bottomSwipedByUser(_ gesture:UISwipeGestureRecognizer) {
+           self.swipeLabel.text = "Down Swipe"
+       }
+    
+    
     @IBAction
     func handleTapGesture(_ gesture: UITapGestureRecognizer){
         print("image tapped")
